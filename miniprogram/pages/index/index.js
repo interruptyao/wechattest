5ffcbfe75877276044c3348992e610e05113d9d5
yaoshungang <924096502@qq.com>
2018-10-02 00:46:49 +0800

    增加画图

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,48 +1,46 @@
-// map.js
+//index.js
+//获取应用实例
+var drawTool = require("../../utils/util.js")
+
+var app = getApp()
 Page({
-  onReady: function (e) {
-    // 使用 wx.createMapContext 获取 map 上下文
-    this.mapCtx = wx.createMapContext('myMap')
-  },
-  getCenterLocation: function () {
-    this.mapCtx.getCenterLocation({
-      success: function (res) {
-        console.log(res.longitude)
-        console.log(res.latitude)
-      }
-    })
-    this.mapCtx.moveToLocation()
+  data: {
+
   },
 
-  moveToLocation: function () {
-    this.mapCtx.moveToLocation()
-  },
-
-  translateMarker: function () {
-    this.mapCtx.translateMarker({
-      markerId: 0,
-      autoRotate: true,
-      duration: 1000,
-      destination: {
-        latitude: 23.10229,
-        longitude: 113.3345211,
-      },
-      animationEnd() {
-        console.log('animation end')
-      }
+  onLoad: function () {
+    var that = this
+    drawTool.drawWithArr(that.data.arr)
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
     })
+
+    
+
+    // wx.request({
+    //   url: 'http://192.168.1.116/phpinfo.php',
+    //   data: {},
+    //   method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    //   // header: {}, // 设置请求的 header
+    //   success: function (res) {
+    //     // success
+    //     console.log(res.data.fundManagers)
+    //   },
+    //   fail: function () {
+    //     // fail
+    //   },
+    //   complete: function () {
+    //     // complete
+    //   }
+    // })
   },
-  
-  includePoints: function () {
-    this.mapCtx.includePoints({
-      padding: [10],
-      points: [{
-        latitude: 23.10229,
-        longitude: 113.3345211,
-      }, {
-        latitude: 23.00229,
-        longitude: 113.3345211,
-      }]
+  toMapFun: function () {
+    wx.navigateTo({
+      url: '../../pages/map/map'　　// 页面 A
     })
   }
 })
