e69b860a66cd1aafb29cc196f5ca5702d0cac8b1
yaoshungang <924096502@qq.com>
2018-10-02 01:11:27 +0800

    二维码
    添加二维码扫描

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,18 +8,18 @@ Page({
 
   },
 
-  onLoad: function () {
+  onLoad: function() {
     var that = this
     drawTool.drawWithArr(that.data.arr)
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
+    app.getUserInfo(function(userInfo) {
       //更新数据
       that.setData({
         userInfo: userInfo
       })
     })
 
-    
+
 
     // wx.request({
     //   url: 'http://192.168.1.116/phpinfo.php',
@@ -38,9 +38,34 @@ Page({
     //   }
     // })
   },
-  toMapFun: function () {
+  toMapFun: function() {
     wx.navigateTo({
-      url: '../../pages/map/map'　　// 页面 A
+      url: '../../pages/map/map'　　 // 页面 A
+    })
+  },
+  getBluetoothDevice: function() {
+    var that = this;
+    var show;
+    wx.scanCode({
+      success: (res) => {
+        this.show = "结果:" + res.result + "二维码类型:" + res.scanType + "字符集:" + res.charSet + "路径:" + res.path;
+        that.setData({
+          show: this.show
+        }) 
+        wx.showToast({
+          title: '成功',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: '失败',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      complete: (res) => {}
     })
   }
 })
